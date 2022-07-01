a2ccca16894e26316d6fcaa4b8b944e1f0f646d8
vmmhep <vmmhep@gmail.com>
2022-07-01 22:53:22 +0200

    HDF5.read explicitly declared

--- src/read.jl
+++ src/read.jl
@@ -7,7 +7,7 @@ using ..filestructure: cyax_file, minfile
 
 function topology(h11::Int,tri::Int,cy::Int=1)
     poly_points::Matrix{Int}, simplices::Matrix{Int} = h5open(cyax_file(h11,tri,cy), "r") do file
-    read(file, "cytools/geometric/points"),read(file, "cytools/geometric/simplices")
+    HDF5.read(file, "cytools/geometric/points"),HDF5.read(file, "cytools/geometric/simplices")
     end
     keys = ["points","simplices"]
     vals = [poly_points, simplices]
@@ -15,14 +15,14 @@ function topology(h11::Int,tri::Int,cy::Int=1)
 end
         
 function geometry(h11::Int,tri::Int,cy::Int=1)
-     h21::Int,
+    h21::Int,
     glsm::Matrix{Int},basis::Vector{Int},
     tip::Vector{Float64},CY_Volume::Float64,divisor_volumes::Vector{Float64},
     Kinv::Matrix{Float64}= h5open(cyax_file(h11,tri,cy), "r") do file
-        read(file, "cytools/geometric/h21"),read(file, "cytools/geometric/glsm"),
-        read(file, "cytools/geometric/basis"),read(file, "cytools/geometric/tip"),
-        read(file, "cytools/geometric/CY_volume"),read(file, "cytools/geometric/divisor_volumes"),
-        read(file, "cytools/geometric/Kinv")
+        HDF5.read(file, "cytools/geometric/h21"),HDF5.read(file, "cytools/geometric/glsm"),
+        HDF5.read(file, "cytools/geometric/basis"),HDF5.read(file, "cytools/geometric/tip"),
+        HDF5.read(file, "cytools/geometric/CY_volume"),HDF5.read(file, "cytools/geometric/divisor_volumes"),
+        HDF5.read(file, "cytools/geometric/Kinv")
     end
     keys = ["h21","glsm_charges","basis","tip","CYvolume","τ_volumes","Kinv"]
     vals = [h21,
@@ -40,8 +40,8 @@ end
 function potential(h11::Int,tri::Int,cy::Int=1)
     L::Matrix{Float64}, Q::Matrix{Int},
     Kinv::Matrix{Float64}= h5open(cyax_file(h11,tri,cy), "r") do file
-        read(file, "cytools/potential/L"),read(file, "cytools/potential/Q"),
-        read(file, "cytools/geometric/Kinv")
+        HDF5.read(file, "cytools/potential/L"),HDF5.read(file, "cytools/potential/Q"),
+        HDF5.read(file, "cytools/geometric/Kinv")
     end
     keys = ["L","Q","K"]
     vals = [L, Q, Hermitian(inv(Kinv))]
@@ -50,14 +50,14 @@ end
 
 function Q(h11::Int,tri::Int,cy::Int=1)
     Q::Matrix{Int} = h5open(cyax_file(h11,tri,cy), "r") do file
-        read(file, "cytools/potential/Q")
+        HDF5.read(file, "cytools/potential/Q")
     end
     return Q
 end
 
 function K(h11::Int,tri::Int,cy::Int=1)
     K::Matrix{Float64} = h5open(cyax_file(h11,tri,cy), "r") do file
-        read(file, "cytools/potential/K")
+        HDF5.read(file, "cytools/potential/K")
     end
     K = 0.5.* (K+transpose(K))
     return Hermitian(K)
@@ -65,14 +65,14 @@ end
 
 function L_log(h11::Int,tri::Int,cy::Int=1)
     L::Matrix{Float64} = h5open(cyax_file(h11,tri,cy), "r") do file
-        read(file, "cytools/potential/L")
+        HDF5.read(file, "cytools/potential/L")
     end
     return L
 end
 
 function L_arb(h11::Int,tri::Int,cy::Int=1)
     L::Matrix{Float64} = h5open(cyax_file(h11,tri,cy), "r") do file
-        read(file, "cytools/potential/L")
+        HDF5.read(file, "cytools/potential/L")
     end
     Ltemp::Vector{ArbFloat} = zeros(ArbFloat,size(L,2))
     @inbounds for i=1:size(L,1)
@@ -83,13 +83,13 @@ end
 
 
 #########################
-##### Read Vacua data ###
+##### HDF5.read Vacua data ###
 #########################
 
 function vacua(h11::Int,tri::Int,cy::Int=1)
     vacua::Int, θparallel_num::Matrix{Int}, θparallel_den::Matrix{Int}, Qtilde::Matrix{Int} = h5open(cyax_file(h11,tri,cy), "r") do file
-    read(file, "vacua/vacua"),read(file, "vacua/thparallel/numerator"),
-        read(file, "vacua/thparallel/denominator"),read(file, "vacua/Qtilde")
+    HDF5.read(file, "vacua/vacua"),HDF5.read(file, "vacua/thparallel/numerator"),
+        HDF5.read(file, "vacua/thparallel/denominator"),HDF5.read(file, "vacua/Qtilde")
     end
     keys = ["vacua","θ∥","Qtilde"]
     vals = [abs(vacua), θparallel_num .// θparallel_den, Qtilde]
@@ -101,8 +101,8 @@ end
 function pq_spectrum(h11::Int,tri::Int,cy::Int=1)
     Hvals::Vector{Float64}, fK::Vector{Float64}, fpert::Vector{Float64} = 
     h5open(cyax_file(h11,tri,cy), "r") do file
-        read(file, "spectrum/Heigvals/log10"),
-        read(file, "spectrum/decay/fK"), read(file, "spectrum/decay/fpert")
+        HDF5.read(file, "spectrum/Heigvals/log10"),
+        HDF5.read(file, "spectrum/decay/fK"), HDF5.read(file, "spectrum/decay/fpert")
     end
     keys = ["m", "fK", "fpert"]
     vals = [Hvals, fK, fpert]
@@ -115,11 +115,11 @@ function hp_spectrum(h11::Int,tri::Int,cy::Int=1)
     quart22_index,quart22_sign::Vector{Int},quart22_log10::Vector{Float64},quart31_index,
     quart31_sign::Vector{Int},
     quart31_log10::Vector{Float64} = h5open(cyax_file(h11,tri,cy), "r") do file
-    read(file, "spectrum/Heigvals/sign"),read(file, "spectrum/Heigvals/log10"),
-    read(file, "spectrum/decay/fK"), read(file, "spectrum/decay/fpert"),read(file, "spectrum/quartdiag/sign"),
-        read(file, "spectrum/quartdiag/log10"),read(file, "spectrum/quart31/index"),read(file, "spectrum/quart31/sign"),
-        read(file, "spectrum/quart31/log10"),read(file, "spectrum/quart22/index"),read(file, "spectrum/quart22/sign"),
-        read(file, "spectrum/quart22/log10")
+    HDF5.read(file, "spectrum/Heigvals/sign"),HDF5.read(file, "spectrum/Heigvals/log10"),
+    HDF5.read(file, "spectrum/decay/fK"), HDF5.read(file, "spectrum/decay/fpert"),HDF5.read(file, "spectrum/quartdiag/sign"),
+        HDF5.read(file, "spectrum/quartdiag/log10"),HDF5.read(file, "spectrum/quart31/index"),HDF5.read(file, "spectrum/quart31/sign"),
+        HDF5.read(file, "spectrum/quart31/log10"),HDF5.read(file, "spectrum/quart22/index"),HDF5.read(file, "spectrum/quart22/sign"),
+        HDF5.read(file, "spectrum/quart22/log10")
     end
     keys = ["msign","m", "fK", "fpert","λselfsign", "λself","λ31_i","λ31sign","λ31", "λ22_i","λ22sign","λ22"]
     vals = [Hsign,Hvals, fK, fpert,quartdiagsign, quartdiaglog,
