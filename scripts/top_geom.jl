5fae111a63272effd8c919892b141c783fa35529
vmmhep <vmmhep@gmail.com>
2022-08-11 11:13:35 +0200

    1000 geometries database

--- scripts/top_geom.jl
+++ scripts/top_geom.jl
@@ -78,7 +78,7 @@ h11_init = 4
 np = nworkers()
 h11_end = 500
 log_files_top = [lfile for i=h11_init:h11_init+h11_end]
-n = [100 for _=h11_init:h11_init+h11_end]
+n = [1000 for _=h11_init:h11_init+h11_end]
 h11 = h11_init:h11_init+h11_end
 CYAxiverse.slurm.writeslurm(CYAxiverse.slurm.jobid,string("There are ", size(h11), "topologies to run.\n"))
 # h11 = shuffle(h11)
