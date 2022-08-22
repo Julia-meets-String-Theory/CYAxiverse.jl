6c8624f9be09bdda7573e6145ce1b6b8cbc41a6e
Documenter.jl <documenter@juliadocs.github.io>
2022-08-22 23:40:59 +0000

    build based on c2ecbcf

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#CYAxiverse.jl","page":"CYAxiverse.jl","title":"CYAxiverse.jl","text":"","category":"section"},{"location":"","page":"CYAxiverse.jl","title":"CYAxiverse.jl","text":"Documentation for CYAxiverse.jl","category":"page"}]
+}
