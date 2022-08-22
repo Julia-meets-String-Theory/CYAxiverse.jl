e4506e83a92b661b2bcfcac3c66630d26870e8e0
vmmhep <vmmhep@gmail.com>
2022-08-22 22:36:26 +0200

    updated docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,18 @@
+push!(LOAD_PATH,"../src/")
 using Documenter
 using CYAxiverse
 
 makedocs(
-    sitename = "CYAxiverse",
-    format = Documenter.HTML(),
+    sitename = "CYAxiverse.jl",
+    authors = "Viraf M. Mehta",
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true",
+        canonical = "https://vmmhep.github.io/CYAxiverse.jl/stable/",
     modules = [CYAxiverse]
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/vmmhep/CYAxiverse.jl.git")
