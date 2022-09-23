452c28b69c77b1a3a674a5019dc0281915a75e10
vmmhep <vmmhep@gmail.com>
2022-09-23 19:35:52 +0200

    Using Revise and Pluto, attempted fix...

--- scripts/testing/pluto.jl
+++ scripts/testing/pluto.jl
@@ -1,3 +1,5 @@
+using Pkg	
+Pkg.activate("/scratch/users/mehta2/cyaxiverse/CYAxiverse")
 using Pluto
 cd("/scratch/users/mehta2/vacua_db")
 Pluto.run(host="0.0.0.0", port=8996, require_secret_for_access=false)
