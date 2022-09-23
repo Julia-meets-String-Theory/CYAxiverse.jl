f646aba983046e3c876228e45ad061541c448890
vmmhep <vmmhep@gmail.com>
2022-09-23 19:36:20 +0200

    Finish db building

--- scripts/top_geom.jl
+++ scripts/top_geom.jl
@@ -79,9 +79,9 @@ GC.gc()
 ##############################
 ############ Main ############
 ##############################
-h11_init = 4
+h11_init = 177
 np = nworkers()
-h11_end = 500
+h11_end = 341
 log_files_top = [lfile for i=h11_init:h11_init+h11_end]
 n = [1000 for _=h11_init:h11_init+h11_end]
 h11 = h11_init:h11_init+h11_end
