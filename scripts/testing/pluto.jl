60ebf3115ef8d59b3997965cfaa673e325d59571
vmmhep <vmmhep@gmail.com>
2022-10-17 18:48:03 +0200

    fixed pluto with revise!

--- scripts/testing/pluto.jl
+++ scripts/testing/pluto.jl
@@ -1,5 +1,6 @@
-using Pkg	
-Pkg.activate("/scratch/users/mehta2/cyaxiverse/CYAxiverse")
+# using Pkg	
+# Pkg.activate("/scratch/users/mehta2/cyaxiverse/CYAxiverse")
+using Revise
 using Pluto
-cd("/scratch/users/mehta2/vacua_db")
+# cd("/scratch/users/mehta2/vacua_db")
 Pluto.run(host="0.0.0.0", port=8996, require_secret_for_access=false)
