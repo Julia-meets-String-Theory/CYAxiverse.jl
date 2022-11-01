0121a688e0be88596393972f4b98b08c88796792
vmmhep <vmmhep@gmail.com>
2022-11-01 10:11:01 +0100

    added qshape functions to generate propagated here

--- scripts/Qeff.jl
+++ scripts/Qeff.jl
@@ -2,8 +2,8 @@
 # Pkg.instantiate()
 
 using Distributed
-using MPIClusterManagers
 import MPI
+using MPIClusterManagers
 # MPI.initialize()
 manager = MPIClusterManagers.start_main_loop(MPI_TRANSPORT_ALL)
 # addprocs(manager)
@@ -16,43 +16,49 @@ else
 end
 
 @everywhere using CYAxiverse
+@everywhere using LinearAlgebra
 
 @everywhere using HDF5
 @everywhere function main_Qshape(h11, tri, cy, l)
     threshold = 1e-2
-    try
-        data = CYAxiverse.generate.LQtildebar(h11, tri, cy; threshold=threshold)
-        if size(data["Qhat"], 1) == size(data["Qhat"], 2)
-            vac = abs(det(data["Qhat"]))
-            return Vector{Int}([1, h11, tri, cy, vac])
-        else
-            vac = floor(sqrt(abs(det(data["Qhat"] * data["Qhat"]'))))
-            return Vector{Int}([0, h11, tri, cy, vac])
-        end
-    catch e
-        open(l, "a") do outf
-            write(outf,string(stacktrace(catch_backtrace()),"--(",h11,",",tri,",",cy,")\n"))
-        end
-        return [0, h11, tri, cy, 0]
-    finally
-        open(l, "a") do outf
-            write(outf,string("vac-(",h11,",",tri,",",cy,")\n"))
+    if isfile(joinpath(CYAxiverse.filestructure.geom_dir(h11,tri,cy),"qshape.h5"))
+    else
+        try
+            CYAxiverse.generate.vacua_estimate_save(h11, tri, cy; threshold=threshold)
+        catch e
+            open(l, "a") do outf
+                write(outf,string(stacktrace(catch_backtrace()),"--(",h11,",",tri,",",cy,")\n"))
+            end
+            if isfile(joinpath(CYAxiverse.filestructure.geom_dir(h11,tri,cy),"qshape.h5"))
+                rm(joinpath(CYAxiverse.filestructure.geom_dir(h11,tri,cy),"qshape.h5"))
+            end
+            h5open(joinpath(CYAxiverse.filestructure.geom_dir(h11,tri,cy),"qshape.h5"), "cw") do f
+                f["square", deflate=9] = 0
+                f["vacua_estimate", deflate=9] = 0
+            end
+        finally
+            open(l, "a") do outf
+                write(outf,string("vac-(",h11,",",tri,",",cy,")\n"))
+            end
         end
     end
 end
 
 
-@everywhere function main_sortQ(h11list::Matrix)
+function main_sortQ(h11list::Matrix)
     Qnon_square = []
     Qsquare = []
-    for i in eachindex(h11list[1, :])
-        if h11list[1, i] == 0 && h11list[end, i] != 0
-            push!(Qnon_square, h11list[2:end-1, i])
-        elseif h11list[1, i] == 1 && h11list[end, i] != 0
-            push!(Qsquare, h11list[2:end-1, i])
+    for col in eachcol(h11list)
+        h11, tri, cy = col
+        data = CYAxiverse.read.qshape(h11, tri, cy)
+        square = data["issquare"]
+        vac = data["vacua_estimate"]
+        if square == 0 && vac != 0
+            push!(Qnon_square, [h11 tri cy vac])
+        elseif square == 1 && vac != 0
+            push!(Qsquare, [h11 tri cy vac])
         end
     end
-    println(Qnon_square)
     if isfile(joinpath(CYAxiverse.filestructure.data_dir(),"Qshape.h5"))
         rm(joinpath(CYAxiverse.filestructure.data_dir(),"Qshape.h5"))
     end
@@ -72,14 +78,14 @@ CYAxiverse.filestructure.logcreate(lfile)
 #### Initialise functions ####
 ##############################
 @time temp_spec = main_Qshape(4,10,1,lfile)
-h11list_temp = [4 4 5 7; 10 11 10 10; 1 1 1 1; lfile lfile lfile lfile]
+h11list_temp = [4 4 5 7; 10 1 5 7; 1 1 1 1; lfile lfile lfile lfile]
 @time begin
-    temp_spec = pmap(main_Qshape, h11list_temp[1,:],h11list_temp[2,:],h11list_temp[3,:], h11list_temp[4,:])
+    temp_vac = pmap(main_Qshape, h11list_temp[1,:],h11list_temp[2,:],h11list_temp[3,:], h11list_temp[4,:])
 end
-println(temp_spec, typeof(temp_spec))
-temp_spec = hcat(temp_spec...)
-@time main_sortQ(temp_spec)
 
+@time begin
+    main_sortQ(h11list_temp[1:3,:])
+end
 # println(temp_geom)
 CYAxiverse.slurm.writeslurm(CYAxiverse.slurm.jobid,string((size(h11list_temp,2)+1), "test runs have finished.\n"))
 ### Clear memory ######
@@ -96,8 +102,10 @@ log_files_spec = [lfile for _=1:size(h11list,2)]
 @time begin
     res = pmap(main_Qshape,h11list[1,:],h11list[2,:],h11list[3,:],log_files_spec)
 end
-res = hcat(res...)
-@time main_sortQ(res)
+
+@time begin
+    main_sortQ(h11list)
+end
 
 
 CYAxiverse.slurm.writeslurm(CYAxiverse.slurm.jobid,string("All workers are done!"))
