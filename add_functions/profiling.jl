ed25d59d9b85f5f081c0a7a7b521dd6fc9a5f5ff
vmmhep <vmmhep@gmail.com>
2022-12-29 22:53:22 +0100

    testing omega functions

--- add_functions/profiling.jl
+++ add_functions/profiling.jl
@@ -2,16 +2,134 @@ module profiling
 
 using HDF5
 using LinearAlgebra
-using ArbNumerics, Tullio, LoopVectorization, Nemo
+using ArbNumerics, Tullio, LoopVectorization, Nemo, SparseArrays
 using GenericLinearAlgebra
 using Distributions
 using TimerOutputs
 
 using ..filestructure: cyax_file, minfile, present_dir
 using ..read: potential
+using ..structs: LQLinearlyIndependent, GeometryIndex, AxionPotential, CanonicalQBasis, ProjectedQ, Projector
+
+
+function LQtilde(Q, L)
+    @assert size(Q, 1) < size(Q, 2) "Looks like you need to transpose..."
+    if @isdefined h11
+    else
+        h11 = size(Q, 2)
+    end
+    @timeit "Q" Q = Matrix{Int}(Q[:, sortperm(L[2,:], rev=true)])
+	@timeit "L" L = L[:, sortperm(L[2,:], rev=true)]
+	@timeit "Qbar init" Qbar = zeros(Int, size(Q,1),1)
+	@timeit "Qtilde init" Qtilde = zeros(Int, size(Q,1),1)
+    @timeit "Lbar init" Lbar = zeros(Int, size(L,1),1)
+	@timeit "Ltilde init" Ltilde = zeros(Int, size(L,1),1)
+	for idx in axes(Q, 2)
+		if rank(hcat(Qtilde, Q[:, idx])) > rank(Qtilde)
+			@timeit "Qtilde" Qtilde = hcat(Qtilde, Q[:, idx])
+            @timeit "Ltilde" Ltilde = hcat(Ltilde, L[:, idx])
+			if rank(Qtilde) == h11
+				break
+			end
+		else
+			@timeit "Qbar1" Qbar = hcat(Qbar, Q[:, idx])
+            @timeit "Lbar1" Lbar = hcat(Lbar, L[:, idx])
+		end
+	end
+    if size(Qtilde, 2) + size(Qbar, 2) != size(Q, 2)
+        @timeit "Qbar2" Qbar = hcat(Qbar[:, 2:end], Q[:, size(Qtilde,2)+size(Qbar,2)-1:end])
+        @timeit "Lbar2" Lbar = hcat(Lbar[:, 2:end], L[:, size(Qtilde,2)+size(Qbar,2):end])
+    end
+    LQLinearlyIndependent(Qtilde[:, 2:end], Qbar, Lbar, Ltilde[:, 2:end])
+end
+
+function αmatrix(LQ::LQLinearlyIndependent; threshold::Float64=0.5)
+    @timeit "Qhat init" Qhat = Matrix{Rational}(LQ.Qtilde)
+    if @isdefined h11
+    else
+        h11 = size(Qhat, 2)
+    end
+    @timeit "Qbar init" Qbar = Matrix{Int}(LQ.Qbar)
+    @timeit "Lhat init" Lhat = LQ.Ltilde
+    @timeit "Lbar init" Lbar = LQ.Lbar
+    @timeit "Ltilde_min" Ltilde_min::Float64 = minimum(@view(Lhat[2,:]))
+    @timeit "Ldiff_limit" Ldiff_limit::Float64 = log10(threshold)
+    @timeit "Qbar threshold" Qbar = @view(Qbar[:, @view(Lbar[2,:]) .>= (Ltilde_min + Ldiff_limit)])
+    @timeit "Lbar threshold" Lbar = @view(Lbar[:, @view(Lbar[2,:]) .>= (Ltilde_min + Ldiff_limit)])
+    @timeit "Qinv" Qinv = (inv(Qhat))
+    @timeit "Qinv eps" Qinv = @.(ifelse(abs(Qinv) < 1e-10, zero(Qinv), round(Qinv; digits=4)))
+    # Qhat::Matrix{Int} = deepcopy(Qtilde)
+    # Lhat = deepcopy(Ltilde)
+    @timeit "αeff init" αeff::Matrix{Rational} = zeros(size(@view(Qhat[:, 1]),1),1)
+    @timeit "α" α::Matrix{Rational} = (Qinv * Qbar)' ##Is this the same as JLM's? YES
+    for i in axes(α,1)
+        for j in axes(α,2)
+            if abs(α[i,j]) > 1e-3
+                @timeit "Ldiff" Ldiff::Float64 = round(Lbar[2,i] - Lhat[2,j], digits=3)
+                if Ldiff > Ldiff_limit
+                else
+                    @timeit "zero α1" α[i,j] = zero(Rational)
+                end
+            else
+                @timeit "zero α1" α[i,j] = zero(Rational)
+            end
+        end
+        if α[i,:] == zeros(size(α,2))
+        else
+            @timeit "Qhat final" Qhat = hcat(Qhat, @view(Qbar[:,i]))
+            @timeit "Lhat final" Lhat = hcat(Lhat, @view(Lbar[:,i]))
+            @timeit "αeff" αeff = hcat(αeff,@view(α[i,:]))
+        end
+        @timeit "αeff final" αeff = hcat(I(h11), αeff)
+    end
+    CanonicalQBasis(Matrix{Int}(Qhat), Matrix{Int}(Qbar), Matrix{Float64}(Lhat), Matrix{Float64}(Lbar), Matrix{Rational}(αeff))
+end
+
+function project_out(v::Vector{T} where T<:Union{Rational{Int64}, Integer})
+    @timeit "I init" idd = Matrix{Rational}(I(size(v,1)))
+    @timeit "norm2" norm2 = dot(v,v)
+    @timeit "projector init" proj = 1 // norm2 * (v * v')
+    @timeit "projector eps" proj = @.(ifelse(abs(proj) < 1e-5, zero(proj), proj))
+    # TODO: #16 Need to remove floating point errors
+    Projector(proj, idd - proj)
+end
+
+function project_out(v::Vector{Float64})
+    reset_timer!
+    @timeit "I init" idd = Matrix{Float64}(I(size(v,1)))
+    @timeit "norm2 float" norm2 = dot(v,v)
+    @timeit "projector init" proj = 1. /norm2 * (v * v')
+    @timeit "projector eps" proj = @.(ifelse(abs(proj) < 1e-5, zero(proj), proj))
+    idd_proj = idd - proj
+    Projector(proj, @.(ifelse(abs(idd_proj) < 1e-5, zero(idd_proj), idd_proj)))
+end
+
+function omega(Ω::Matrix{Int})
+    reset_timer!
+    if @isdefined h11
+    else
+        h11 = size(Ω, 2)
+    end
+    @timeit "Ωperp init" Ωperp = Matrix{Rational}(deepcopy(Ω))
+    @timeit "Ωparallel init" Ωparallel = []
+    for (i, col) in enumerate(eachcol(Ω))
+        # TODO: #15 Π function
+        proj = project_out(Vector(col))
+        @timeit "Ωperp" Ωperp[:, i+1:end] = proj.Πperp * Ωperp[:, i+1:end]
+        @timeit "Ωperp eps" Ωperp = @.(ifelse(abs(Ωperp) < 1e-5, zero(Ωperp), Ωperp))
+        if i < h11
+            @timeit "Ωparallel1" push!(Ωparallel, vcat(zeros(Float64, i), mapslices(norm, proj.Π * Ω[:, i+1:end]; dims=1)'))
+        end
+    end
+    #TODO #49: check construction
+    @timeit "Ωparallel2" Ωparallel = hcat(zeros(h11), Ωparallel...)
+    @timeit "Ωparallel eps" Ωparallel = @.(ifelse(abs(Ωparallel) < 1e-5, zero(Ωparallel), Ωparallel))
+    print_timer()
+    ProjectedQ(sparse(Ωperp), sparse(Ωparallel))
+end
 
 function vacua(L::Matrix{Float64},Q::Matrix{Int})
-    reset_timer!()
+    reset_timer!
     @timeit "h11" h11::Int = size(Q,2)
     if h11 < 50
         ###### Nemo SNF #####
