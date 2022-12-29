96182cd726bd8090c514acf797e968f03b9efb1c
vmmhep <vmmhep@gmail.com>
2022-12-29 22:53:40 +0100

    continued tree structure testing

--- notebooks/tree_testing.jl
+++ notebooks/tree_testing.jl
@@ -11,7 +11,7 @@ using Pkg;
 Pkg.activate("/scratch/users/mehta2/cyaxiverse/CYAxiverse");
 
 # ╔═╡ 0227789b-1548-4d6a-ad2f-ef017530849b
-using PlutoUI, HDF5, ArbNumerics, LineSearches, Optim, CairoMakie, Distributions, LinearAlgebra, ProgressLogging, Revise, Random, SparseArrays, LeftChildRightSiblingTrees
+using PlutoUI, HDF5, ArbNumerics, LineSearches, Optim, CairoMakie, Distributions, LinearAlgebra, ProgressLogging, Revise, Random, SparseArrays, LeftChildRightSiblingTrees, Profile
 
 
 # ╔═╡ ce668813-d11a-4dc0-8f0b-3c3dcdd039f3
@@ -98,16 +98,48 @@ t.subtrees[3].subtrees[1].data
 # ╔═╡ 2edfe043-9a4a-4e64-a776-4d329ff05b91
 lqhat = CYAxiverse.generate.:αmatrix(CYAxiverse.structs.GeometryIndex(h11=10,polytope=10,frst=1))
 
-# ╔═╡ 9620b336-cbd8-4e64-b3f0-2790bd7fbcb2
-
+# ╔═╡ 3eb199d9-8883-4755-9295-a0ef66816d99
+function project_out(v::Vector{Int})
+    idd = Matrix{Rational}(I(size(v,1)))
+    norm2::Int = dot(v,v)
+    proj = 1 // norm2 * (v * v')
+    Projector(@.(ifelse(abs(proj) < eps(), zero(proj), proj)), idd - proj)
+end
 
 # ╔═╡ 4e282b9f-9c58-4d46-a2a7-ab6ed21c87e0
 CYAxiverse.generate.omega(lqhat.Qhat).Ωparallel
 
 # ╔═╡ e3ceb403-cb73-4195-a717-ad7f7f2d14a9
 begin
-	Ωperp = CYAxiverse.generate.omega(lqhat.Qhat).Ωperp
-	[max(2π / norm(Ωperp[:,i]), 2π) for i in axes(Ωperp, 2)]
+	Ω = CYAxiverse.generate.omega(lqhat.Qhat)
+	Ωperp = Ω.Ωperp
+end
+
+# ╔═╡ 36610454-1c4b-4ffb-8d4c-8c4d5160ce96
+Ωperp
+
+# ╔═╡ ed3c16c0-4632-4695-8c39-0325631002dd
+lcm(denominator.([1//4, 1//6, 1//8]))
+
+# ╔═╡ 1572e225-6922-4231-9f1f-e5d54f2f8560
+@time begin
+	geom_idx = CYAxiverse.structs.GeometryIndex(h11 = 10, polytope = 10, frst = 1)
+	@profile CYAxiverse.generate.norm2(geom_idx)
+	Profile.print()
+end
+	
+
+# ╔═╡ 9620b336-cbd8-4e64-b3f0-2790bd7fbcb2
+@time begin
+	pot_data = CYAxiverse.generate.potential(geom_idx)
+	Q = Matrix{Int}(pot_data.Q')
+	L = Matrix{Float64}(pot_data.L')
+	CYAxiverse.profiling.omega(CYAxiverse.profiling.:αmatrix(CYAxiverse.profiling.LQtilde(Q, L); threshold = 1e-2).Qhat)
+end
+
+# ╔═╡ 598478a3-cc79-4c40-9003-f7ca00db49a2
+@time begin
+	norm2minus1Ωperp = CYAxiverse.generate.norm2(Ωperp)
 end
 
 # ╔═╡ 04222ee1-0f18-4b41-9900-b19ef9f32e57
@@ -121,7 +153,7 @@ end
 @time vcat(mapslices(norm, CYAxiverse.generate.project_out(Vector(lqhat.Qhat[:,3])).Π * lqhat.Qhat[:, 3+1:end]; dims=1)', zeros(3))
 
 # ╔═╡ 09b8c30c-3393-4d32-b89b-c2bd01d57ee1
-sqrt(2//25)
+norm([1//5, -1//5]), sqrt(2)/5
 
 # ╔═╡ Cell order:
 # ╟─69dea65d-54ce-4abd-8476-6fe481cdff57
@@ -142,9 +174,14 @@ sqrt(2//25)
 # ╠═b15c87a6-c028-4ad6-83bc-23ac7840c260
 # ╠═47473bda-1619-499a-96ba-cb44e9b28a55
 # ╠═2edfe043-9a4a-4e64-a776-4d329ff05b91
+# ╠═3eb199d9-8883-4755-9295-a0ef66816d99
 # ╠═9620b336-cbd8-4e64-b3f0-2790bd7fbcb2
 # ╠═4e282b9f-9c58-4d46-a2a7-ab6ed21c87e0
 # ╠═e3ceb403-cb73-4195-a717-ad7f7f2d14a9
+# ╠═36610454-1c4b-4ffb-8d4c-8c4d5160ce96
+# ╠═ed3c16c0-4632-4695-8c39-0325631002dd
+# ╠═1572e225-6922-4231-9f1f-e5d54f2f8560
+# ╠═598478a3-cc79-4c40-9003-f7ca00db49a2
 # ╠═04222ee1-0f18-4b41-9900-b19ef9f32e57
 # ╠═a6c45afe-c9a6-421b-8414-731e6ba0971b
 # ╠═09b8c30c-3393-4d32-b89b-c2bd01d57ee1
