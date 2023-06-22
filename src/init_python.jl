6705375598ff22db81979c9aec5d196a5b16c3eb
vmmhep <vmmhep@gmail.com>
2023-06-22 18:45:56 +0200

    checks PYTHONPATH is set

--- src/init_python.jl
+++ src/init_python.jl
@@ -1,9 +1,16 @@
 using Pkg
 if haskey(ENV, "PYTHON")
-    if ENV["PYTHON"] == "/scratch/users/mehta2/cyaxiverse_python/bin/python"
+    if ENV["PYTHON"] == "/home/cytools/cytools-venv//bin/python"
+        using PyCall
+        if PyCall.current_python() != ENV["PYTHON"]
+            Pkg.build("PyCall")
+        end
+    elseif ENV["PYTHON"] == "/scratch/users/mehta2/cyaxiverse_python/bin/python"
+        using PyCall
+        if PyCall.current_python() != ENV["PYTHON"]
+            Pkg.build("PyCall")
+        end
     else
-        ENV["PYTHON"] = "/scratch/users/mehta2/cyaxiverse_python/bin/python"
-        Pkg.build("PyCall")
-        
+        error("Please set ENV['PYTHON'] with your PYTHONPATH")
     end
 end
