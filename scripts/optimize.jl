588e88c991eed75c92cbe638410a2a2d902b272b
vmmhep <vmmhep@gmail.com>
2023-07-20 15:49:48 +0200

    minimise small geometries

--- scripts/optimize.jl
+++ scripts/optimize.jl
@@ -82,7 +82,7 @@ GC.gc()
 Random.seed!(1234567890)
 h11list = CYAxiverse.filestructure.paths_cy()[2]
 # h11list = h11list[:, h11list[1, :] .!= 491]
-h11list = h11list[:, h11list[1, :] .== 225 .|| h11list[1, :] .== 249 .|| h11list[1, :] .== 252 .|| h11list[1, :] .== 254]
+h11list = h11list[:, h11list[1, :] .== 1 .|| h11list[1, :] .== 2 .|| h11list[1, :] .== 3]
 geom_params = [CYAxiverse.structs.GeometryIndex(col...) for col in eachcol(h11list)]
 geom_params = shuffle!(geom_params)
 
