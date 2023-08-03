a120e3ef697791c0cae245b56fa3b4b9727ed938
vmmhep <vmmhep@gmail.com>
2023-08-03 10:51:26 +0200

    added despatch: minimize(Q, L; random_phase, threshold)

--- src/jlm_minimizer.jl
+++ src/jlm_minimizer.jl
@@ -68,6 +68,56 @@ function minimize(geom_idx::GeometryIndex; random_phase=false, threshold = 0.01)
     end
 end
 
+function minimize(Q::Matrix{Int}, L::Matrix{Float64}; random_phase=false, threshold = 0.01)
+    αtest = αmatrix(Q, L; threshold=threshold)
+    if typeof(αtest)<:Canonicalα
+        Qtilde = LQtilde(Q, L).Qtilde
+        det_Q_tilde = Int(abs(round(det(Qtilde))))
+        n_axions = size(αtest.α[αtest.αrowmask, αtest.αcolmask], 1)
+        Q_reduced = hcat(1//1 * I(n_axions), αtest.α_complete[αtest.αrowmask, αtest.αcolmask])'
+        Q_reduced_temp = hcat(1//1 * I(n_axions), αtest.α[αtest.αrowmask, αtest.αcolmask])'
+        for (i,row) in enumerate(eachrow(Q_reduced[n_axions+1:end, :]))
+            if sum(abs.(row) .== 0) == (size(row, 1) - 1)
+                Q_reduced_temp[n_axions+i, :] .= 0
+            end
+        end
+        Qrowmask = [any(row .!= 0) for row in eachrow(Q_reduced_temp)]
+        Q_reduced_temp = Q_reduced_temp[Qrowmask, :]
+        if size(Q_reduced_temp, 1) == size(Q_reduced_temp, 2)
+            return Min_JLM_Square(det_Q_tilde, Int(floor(sqrt(abs(det(αtest.Qhat * αtest.Qhat'))))))
+        else
+            phase_vector = phase(size(Qtilde, 2), αtest)
+            if random_phase
+                phase_vector = mod.(phase_vector .+ rand(Uniform(0, 2π), size(phase_vector, 1)), 2π)
+            end
+            L_reduced = Matrix(hcat(αtest.Lhat[:, axes(Qtilde, 2)][:, αtest.αrowmask], αtest.Lhat[:, size(Qtilde, 2)+1:end][:, αtest.αcolmask])')
+            # L_reduced = L_reduced[Qrowmask, :]
+            flag_int = ifelse(maximum(denominator.(Matrix(Q_reduced))) == 1, 1, 0)
+            αrescaled = Matrix{Integer}(det_Q_tilde .* Matrix(Q_reduced))
+            θparallel = vacua_SNF(αrescaled).:θparallel .* Rational(det_Q_tilde)
+            basis_inverse = []
+            if abs(maximum(denominator.(θparallel)) * maximum(numerator.(abs.(θparallel)))) > 2^60
+                θparallel::Matrix{Rational{BigInt}} = θparallel
+                basis_inverse = ifelse(size(inv(θparallel)) == (1,1), Rational{BigInt}(inv(θparallel)[1,1]), Matrix{Rational{BigInt}}(inv(θparallel)))
+            else
+                basis_inverse = ifelse(size(inv(θparallel)) == (1,1), Rational(inv(θparallel)[1,1]), Matrix{Rational}(inv(θparallel)))
+            end
+            vol_basis = Rational(det(θparallel))
+            if size(Q_reduced, 2) == 1
+                to_solve1D = Solver1D(10π, Float64.(vec(Q_reduced)), L_reduced[:, 2], L_reduced[:, 1], det_Q_tilde, phase_vector, flag_int, basis_inverse, vol_basis)
+                return one_dim_axion_solver(to_solve1D)
+                # return to_solve1D
+            else
+                to_solveND = SolverND(100_000, Float64.(Matrix(Q_reduced)), L_reduced[:, 2], L_reduced[:, 1], det_Q_tilde, phase_vector, flag_int, basis_inverse, vol_basis)
+                return multi_axion_solver(to_solveND)
+                # return to_solveND
+            end
+        end
+    else
+        return Min_JLM_Square(Int(abs(round(det(αtest.Qhat)))), Int(floor(sqrt(abs(det(αtest.Qhat * αtest.Qhat'))))))
+    end
+end
+
 function minimize_save(geom_idx::GeometryIndex; random_phase=false, threshold = 0.01)
     min_data = minimize(geom_idx; random_phase=random_phase, threshold = threshold)
     if isfile(minfile(geom_idx))
